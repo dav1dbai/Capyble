79af80a2e7b510af585098c30f810dbd2f0bc918
wangshei <wangshei@usc.edu>
2024-04-21 03:02:36 -0700

    calendar back and next

--- capy/src/components/calendar.jsx
+++ capy/src/components/calendar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const Calendar = () => {
   const [events, setEvents] = useState([]);
+  const [currentDate, setCurrentDate] = useState(new Date());
 
   useEffect(() => {
     fetchEvents();
@@ -17,16 +18,27 @@ const Calendar = () => {
     }
   };
 
+  const nextDate = () => {
+    const newDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
+    setCurrentDate(new Date(newDate)); // Update state to the next day
+  };
+
+  const lastDate = () => {
+    const newDate = new Date(currentDate.setDate(currentDate.getDate() - 1));
+    setCurrentDate(new Date(newDate)); // Update state to the next day
+  };
+
   return (
     <div >
       <style>{'body { background-color: #F8DEC1; }'}</style>
       <div className='text-lg text-tab_border_brown font-mitr 'style={{marginTop:"-20px", marginLeft:"20px"}}>
         David's Calendar
       </div>
-      <h2 className='text-lg text-text_brown font-mitr ' style={{position: "fixed", left: "37%",top: "12%", fontSize:"15px"}}><strong>{getDate()}</strong></h2>
+      <h2 className='text-lg text-text_brown font-mitr ' style={{position: "fixed", left: "37%",top: "12%", fontSize:"15px"}}><strong>{currentDate.toLocaleDateString()}</strong></h2>
       {events.length === 0 ? (
         <p>No upcoming events found.</p>
       ) : (
+
         <ul>
           <br />
           {events.map((event) => (
@@ -46,6 +58,11 @@ const Calendar = () => {
         </ul>
         
       )}
+      <div  style={{position: "fixed", left: "35%",top: "90%", fontSize:"15px"}}>
+      <button onClick={lastDate}>Back |</button> {" "}
+      <button onClick={nextDate}>| Next</button>
+
+      </div>
     </div>
   );
 };
@@ -69,6 +86,26 @@ function getDate() {
   const year = today.getFullYear();
   const date = today.getDate();
   return `${month}/${date}/${year}`;
+}
+
+
+
+function todayList(events) {
+
+  // Prepare list items using a for loop
+  let todayevents = [];
+  let todayevents_start = [];
+  let now = new Date()
+  for (let i = 0; i < events.length; i++) {
+    if(events.start.substr(0, 12) == now){
+      todayevents.push(events[i].summary);
+      todayevents_start.push(events[i].start);
+    }
+  }
+
+  return (
+    <ul></ul>
+  );
 }
 
 export default Calendar;
